720ed3722f212d6499d73e4a79e090f67b449a1c
Okoronkwo Chibueze <ricobuez@gmail.com>
2021-06-10 23:09:00 +0100

    finished creating the form

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php
     $mat_no = $_POST['mat_no'];
-    $full_name = $_POST['full_name'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
     $email = $_POST['email'];
     $phone_number = $_POST['phone_number'];
     $level = $_POST['level'];
@@ -10,10 +11,10 @@
      if ($conn->connect_error){
          die('Connection Failed : '.$conn->connect_error);
      }else{
-         $stmt = $conn->prepare("insert into students(mat_no, full_name, email, phone_number, level, sex) values(?,?,?,?,?,?)"); 
-         $stmt->bind_param("sssiis",$mat_no, $full_name, $email, $phone_number, $level, $sex);
+         $stmt = $conn->prepare("insert into students(mat_no, first_name, last_name, email, phone_number, level, sex) values(?,?,?,?,?,?,?)"); 
+         $stmt->bind_param("ssssiis", $mat_no, $first_name, $last_name, $email, $phone_number, $level, $sex);
          $stmt->execute();
-         echo ('You have successfully registered '.$full_name.' as a student.');
+         echo 'YOU HAVE SUCCESSFULLY REGISTERED '. $first_name . ' AS A STUDENT';
          $stmt->close();
          $conn->close();
      }
